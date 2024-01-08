02684d9451e620a034363d946711a147ba0bc9c7
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-01-08 02:31:25 -0400

    chore: standarize development workspace

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Generate PKGBUILD from cargo";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    pre-commit-hooks,
+    ...
+  }: let
+    name = "cargo-pkgbuild-action";
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        system,
+        ...
+      }: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {};
+        };
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra.enable = true;
+            editorconfig-checker.enable = true;
+            shellcheck.enable = true;
+            yamllint.enable = true;
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          name = "devShell";
+          inherit (pre-commit-check) shellHook;
+          buildInputs = with pre-commit-hooks.packages.${system}; [
+            alejandra
+            editorconfig-checker
+          ];
+          packages = with pkgs; [
+            docker
+            nodePackages.dockerfile-language-server-nodejs
+            nodePackages.bash-language-server
+            nodePackages.yaml-language-server
+          ];
+        };
+      in {
+        devShells = {
+          default = devShell;
+        };
+
+        checks = {
+          formatting = pre-commit-check;
+        };
+      };
+    };
+}
